f797d3003d155e4de5af6f5fe6e8b43e338b11d0
benjaminng <shen_soonng@hotmail.com>
2020-06-07 11:04:30 +0800

    seem to solve go another page but cannot record issue

--- background.js
+++ background.js
@@ -4,6 +4,14 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.storage.sync.get(['recordingState'],function(recorder){
+    if (recorder.recordingState === 'RECORDING') {
+      chrome.tabs.sendMessage(tabId, {"message": "clicked_browser_action"});
+    }
+  });
+});
+
 function handleMessage(message) {
   if (message.action === 'url') {
     lastUrl = message.value
@@ -12,6 +20,6 @@ function handleMessage(message) {
       let recording = recorder.recording;
       recording.push(message);
       chrome.storage.sync.set({'recording': recording});
-  });
+    });
   }
 }
